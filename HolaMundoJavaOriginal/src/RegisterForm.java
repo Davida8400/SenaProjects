a96ac597b41aaed426627f3cbbc384ed078df1b1
Jhohan David Pedraza Urrego <118397525+Davida8400@users.noreply.github.com>
2023-02-19 19:09:41 -0500

    Se añadió el folder HolaMundoJavaOriginal

---
+++ HolaMundoJavaOriginal/src/RegisterForm.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class RegisterForm {
+    private JTextField tfName;
+    private JTextField tfLastName;
+    private JTextField tfAddress;
+    private JTextField tfPhone;
+    private JPasswordField pfPassword;
+    private JButton btnRegister;
+    private JButton btnCancel;
+    private JRadioButton rbtnMale;
+    private JRadioButton rbtnFemale;
+    private JTextField tfEmail;
+    private JTextField tfAge;
+    private JPasswordField pfConfirmPassword;
+}
