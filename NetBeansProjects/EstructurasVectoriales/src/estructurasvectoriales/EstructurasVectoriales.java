ba8cf58159840339b639a805bf5783194b8e6165
Davida8400 <jhohan_pedraza@soy.sena.edu.co>
2023-03-10 00:44:16 -0500

    Se añadieron comentarios

--- NetBeansProjects/EstructurasVectoriales/src/estructurasvectoriales/EstructurasVectoriales.java
+++ NetBeansProjects/EstructurasVectoriales/src/estructurasvectoriales/EstructurasVectoriales.java
@@ -1,35 +1,30 @@
 package estructurasvectoriales;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JLabel;
-
 /**
- *
  * @author David
  */
-public class EstructurasVectoriales implements ActionListener{
-    JLabel jlChi, jlVzl, jlUSA, jlArg, jlCol, jlJpn, jlRus, jlInd;
-    
+public class EstructurasVectoriales {
     public void Matriz(){
-        String Paises [] = {"China", "Venezuela", "Estados Unidos", "Argentina", "Colombia", "Japón", "Rusia", "India"};
-        String Ciudades[]={"Santiago de Cali", "Bogotá", "Santa Marta", "Barranquilla", "Villavicencio", "Medellín"};
-        int cantidad = Ciudades.length;
-        for(int i = 0; i < cantidad; i++){
-            System.out.println(Ciudades[i]);
+        String Estudiante[][]={
+            {"1010899023", "Luis Antonio Valderrama Orjuela", "4.8", "2.9"},
+            {"1010899035", "Diana Marcea Céspedes Maldonado", "5.0", "4.9"},
+            {"1011235694", "Juan Pablo Miranda Pacheco", "2.9", "1.0"}
+        };
+        int filas = Estudiante.length;
+        int columnas = Estudiante[0].length;
+        //la variable i es para las filas
+        for (int i = 0; i < filas; i++) {
+            //la variable j es para las columnas
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(Estudiante[i][j] + " ");
+            }
+            // Estudiante[i][2] significa que accede al indice (2) de la matriz. (0, 1, 2).
+            // Estudiante[i][3] significa que accede al indice (3) de la matriz. (0, 1, 2, 3).
+            //Esto es porque los números están ubicados en esa posición.
+            System.out.println("El promedio es: " + (Double.parseDouble(Estudiante[i][2]) + Double.parseDouble(Estudiante[i][3])) / 2);
         }
     }
     public static void main(String[] args) {
         EstructurasVectoriales Carga = new EstructurasVectoriales();
         Carga.Matriz();
-    }
-
-    /**
-     *
-     * @param e
-     */
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
     }
 }
