969c8f000f5168e54b92fd8062962da70e9f2be5
Davida8400 <jhohan_pedraza@soy.sena.edu.co>
2023-05-07 20:47:04 -0500

    Se eliminaron carpetas innecesarias, y se creó PyProjects

--- AerolineaDavidGUI/src/User.java
+++
@@ -1,9 +0,0 @@
-public class User {
-    public String name;
-    public String last_name;
-    public String address;
-    public String phone;
-    public String email;
-    public String password;
-    public String gender;
-}
